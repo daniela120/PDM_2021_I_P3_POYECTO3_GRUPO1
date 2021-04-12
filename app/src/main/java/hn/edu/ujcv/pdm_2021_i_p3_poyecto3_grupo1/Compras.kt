18fdb0ba07302dbeff4f93ca6a9a608a12dc23b4
Miguel <miguel.torres@ujcv.edu.hn>
2021-04-12 14:31:17 -0600

    fun Regresar

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Compras.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Compras.kt
@@ -1,13 +1,19 @@
 package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_compras.*
 
 class Compras : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compras)
+        btn_RegresarCompras.setOnClickListener { Regresar() }
 
-
+    }
+    private fun Regresar() {
+        val intent = Intent(this, Menu::class.java)
+        startActivity(intent)
     }
 }
