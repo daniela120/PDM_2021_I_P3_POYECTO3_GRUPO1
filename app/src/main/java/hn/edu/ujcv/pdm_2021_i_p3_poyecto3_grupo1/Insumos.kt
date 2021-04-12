f140811d2dc4034ef4bbde4a82ed2652763e6008
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-12 17:26:58 -0600

    Validaciones Insumos e Inventario

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Insumos.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Insumos.kt
@@ -3,6 +3,8 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_departamento.*
 import kotlinx.android.synthetic.main.activity_insumos.*
 
 class Insumos : AppCompatActivity() {
@@ -10,9 +12,30 @@ class Insumos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insumos)
         btn_RegresarInsumos.setOnClickListener { Regresar() }
+        btn_LimpiarInsum.setOnClickListener { guardar() }
     }
     private fun Regresar() {
         val intent = Intent(this, Menu::class.java)
         startActivity(intent)
     }
+
+
+    private  fun guardar() {
+
+        if (txt_IdInsumo.text.toString().isEmpty()) {
+            Toast.makeText(this, "Ingrese ID del Insumo", Toast.LENGTH_SHORT).show()
+        }else {
+            if (txt_NombreInsumo.text.toString().isEmpty()) {
+                Toast.makeText(this, "Ingrese un Nombre de Insumo", Toast.LENGTH_SHORT).show()
+            } else {
+                if (txt_TipoCompra_Insumo.text.toString().isEmpty()) {
+                    Toast.makeText(this, "Ingrese una Tipo de Compra", Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(this, "Realizada con exito!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Inventario.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Inventario.kt
@@ -3,6 +3,8 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_insumos.*
 import kotlinx.android.synthetic.main.activity_inventario.*
 
 class Inventario : AppCompatActivity() {
@@ -10,10 +12,27 @@ class Inventario : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventario)
         btn_RegresarInventario.setOnClickListener { Regresar() }
+        btn_LimpiarInv.setOnClickListener { guardar() }
 
     }
     private fun Regresar() {
         val intent = Intent(this, Menu::class.java)
         startActivity(intent)
+    }
+    private  fun guardar() {
+
+        if (txt_IdAlmacen_Inventario.text.toString().isEmpty()) {
+            Toast.makeText(this, "Ingrese ID del Inventario", Toast.LENGTH_SHORT).show()
+        }else {
+            if (txt_IdProduccion_Inventario.text.toString().isEmpty()) {
+                Toast.makeText(this, "Ingrese un Id de Produccion", Toast.LENGTH_SHORT).show()
+            } else {
+                if (txt_Cantidad.text.toString().isEmpty()) {
+                    Toast.makeText(this, "Ingrese una Cantidad", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(this, "Realizada con exito!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
