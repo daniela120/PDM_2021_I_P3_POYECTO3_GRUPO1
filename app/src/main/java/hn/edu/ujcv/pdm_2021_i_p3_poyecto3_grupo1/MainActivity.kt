61dc9c2850cf8034d438eb282b8dbca624b5e9be
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-12 16:38:30 -0600

    autenticacion login

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MainActivity.kt
@@ -3,6 +3,7 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,24 @@ class MainActivity : AppCompatActivity() {
         btn_login.setOnClickListener { Pruebaregistrar() }
     }
     private fun Pruebaregistrar() {
+        if (txt_Usuario.text.toString()=="Helen Galo" && txt_Password.text.toString()=="helen" ) {
             val intent = Intent(this, Menu::class.java)
             startActivity(intent)
+        } else {
+            if (txt_Usuario.text.toString()=="Edwin Espino" && txt_Password.text.toString()=="edwin" ) {
+                val intent = Intent(this, Menu::class.java)
+                startActivity(intent)
+            } else {
+                if (txt_Usuario.text.toString()=="Miguel Torres" && txt_Password.text.toString()=="miguel" ) {
+                    val intent = Intent(this, Menu::class.java)
+                    startActivity(intent)
+                } else {
+                    if (txt_Usuario.text.toString() == "Daniela Herrera" && txt_Password.text.toString() == "daniela") {
+                        val intent = Intent(this, Menu::class.java)
+                        startActivity(intent)
+                    } else {
 
-    }
+                        Toast.makeText(this, "Ingrese un usuario valido ", Toast.LENGTH_SHORT).show()
+                    }
+                }}}}
 }
