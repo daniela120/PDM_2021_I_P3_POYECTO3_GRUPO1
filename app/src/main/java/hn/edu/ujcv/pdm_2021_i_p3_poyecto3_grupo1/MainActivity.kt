59fc08b9a4d65f7630210b7f9e27321251b3e48c
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-11 20:36:05 -0600

    .

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MainActivity.kt
@@ -1,11 +1,19 @@
 package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_login.setOnClickListener { Pruebaregistrar() }
+    }
+    private fun Pruebaregistrar() {
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+
     }
 }
