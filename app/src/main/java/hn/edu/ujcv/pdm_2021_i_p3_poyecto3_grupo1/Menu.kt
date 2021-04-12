b929e21a848ed086469d79ac22ca1f533d9aefd8
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-11 19:01:55 -0600

    MENU

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Menu.kt
@@ -0,0 +1,12 @@
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
