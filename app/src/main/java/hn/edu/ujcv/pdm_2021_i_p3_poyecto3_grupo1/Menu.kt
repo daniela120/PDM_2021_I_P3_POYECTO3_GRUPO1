7e0ed15fbf74a14372025964fc4be8d4730a2515
Miguel <miguel.torres@ujcv.edu.hn>
2021-04-11 19:07:05 -0600

    Fondo

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Menu.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Menu.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_menu)
 
     }
 }
