7bb5087464bfc1b867f57832a64b27cb1868dcc6
Miguel <miguel.torres@ujcv.edu.hn>
2021-04-12 17:29:29 -0600

    toast venta

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Ventas.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Ventas.kt
@@ -3,6 +3,8 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_departamento.*
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.activity_ventas.*
 
@@ -11,6 +13,52 @@ class Ventas : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventas)
         btn_RegresarVen.setOnClickListener { Regresar()}
+    }
+
+    private  fun guardar() {
+
+        if (txt_VentaId.text.toString().isEmpty()) {
+            Toast.makeText(this, "Ingrese ID de Venta", Toast.LENGTH_SHORT).show()
+        } else {
+            if (txt_IDProductoV.text.toString().isEmpty()) {
+                Toast.makeText(this, "Ingrese ID de Producto", Toast.LENGTH_SHORT).show()
+            } else {
+                if (txt_IDEmpleadoV.text.toString().isEmpty()) {
+                    Toast.makeText(this, "Ingrese ID de Empleado", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (txt_IDCLienteV.text.toString().isEmpty()) {
+                        Toast.makeText(this, "Ingrese ID de Cliente", Toast.LENGTH_SHORT).show()
+                        if (txt_Nombre.text.toString().isEmpty()) {
+                            Toast.makeText(this, "Ingrese Nombre de Cliente", Toast.LENGTH_SHORT).show()
+                        } else {
+                            if (txt_DescripcionV.text.toString().isEmpty()) {
+                                Toast.makeText(this, "Ingrese Una descripcion",Toast.LENGTH_SHORT).show()
+                            } else {
+                                if (txt_CantidadV.text.toString().isEmpty()) {
+                                    Toast.makeText(this, "Ingrese una Cantidad", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    if (txt_PrecioV.text.toString().isEmpty()) {
+                                        Toast.makeText(this, "Ingrese un Precio", Toast.LENGTH_SHORT).show()
+                                    } else {
+                                        if (txt_SubtotalV.text.toString().isEmpty()) {
+                                            Toast.makeText(this, "Ingrese un Subtotal", Toast.LENGTH_SHORT).show()
+                                            if (txt_ISVVenta.text.toString().isEmpty()) {
+                                                Toast.makeText(this, "Ingrese ISV", Toast.LENGTH_SHORT).show()
+                                            }else{
+                                                if(txt_TotalV.text.toString().isEmpty()){
+                                                    Toast.makeText(this, "Ingrese EL Total a Pagar", Toast.LENGTH_SHORT).show()
+                                                }
+                                            Toast.makeText(this, "Realizada con exito!", Toast.LENGTH_SHORT).show()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     private fun Regresar() {
         val intent = Intent(this, Menu::class.java)
