80c1e2455ee1c76210c28149f86404fdf27c9aba
Miguel <miguel.torres@ujcv.edu.hn>
2021-04-11 18:47:44 -0600

    rename

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Costos.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Costos.kt
@@ -3,10 +3,10 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity12 : AppCompatActivity() {
+class Costos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main12)
+        setContentView(R.layout.activity_costos)
 
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Insumos.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Insumos.kt
@@ -3,10 +3,11 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity10 : AppCompatActivity() {
+class Insumos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main10)
+        setContentView(R.layout.activity_insumos)
 
     }
+
 }
