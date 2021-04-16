7ba9b84db83e19069d3769d197197787cbea43b1
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-16 13:06:27 -0600

    SERVICIOS

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Empleado.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Empleado.kt
@@ -3,14 +3,21 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.PersonDataCollectionItem
 import kotlinx.android.synthetic.main.activity_empleado.*
 import kotlinx.android.synthetic.main.activity_productos.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Empleado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empleado)
+        val botonGetId = findViewById<Button>(R.id.btn_BuscarEmpleado)
+        botonGetId.setOnClickListener {v -> callServiceGetPerson()}
 
     }
 
@@ -43,4 +50,23 @@ class Empleado : AppCompatActivity() {
         val intent = Intent(this, Menu::class.java)
         startActivity(intent)
     }*/
+
+    private fun callServiceGetPerson() {
+        val personService:PersonService = RestEngine.buildService().create(PersonService::class.java)
+        var result: Call<PersonDataCollectionItem> = personService.getPersonById(1)
+
+        result.enqueue(object : Callback<PersonDataCollectionItem> {
+            override fun onFailure(call: Call<PersonDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@Empleado,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                call: Call<PersonDataCollectionItem>,
+                response: Response<PersonDataCollectionItem>
+            ) {
+                Toast.makeText(this@Empleado,"OK"+response.body()!!.nombrecompleto,Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
 }
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/entities/ClienteDataCollection.kt
@@ -0,0 +1,4 @@
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities
+
+class ClienteDataCollection {
+}
