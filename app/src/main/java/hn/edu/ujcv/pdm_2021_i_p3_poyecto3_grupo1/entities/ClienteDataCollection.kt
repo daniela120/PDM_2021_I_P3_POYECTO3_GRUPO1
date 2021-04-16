ca17182c4fc158bdec04772b115c4b99e170a1eb
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-16 13:38:31 -0600

    SERVICIOS 1

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/entities/ClienteDataCollection.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/entities/ClienteDataCollection.kt
@@ -1,4 +1,13 @@
 package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities
 
-class ClienteDataCollection {
-}
+class ClienteDataCollection:ArrayList<ClienteDataCollectionItem>()
+
+data class ClienteDataCollectionItem(
+    val id:Long?,
+    val nombrecompleto:Long,
+    val telefono: String,
+    val correo: String,
+    val direccion: String,
+    val dni:String,
+    val rtn: String
+)
