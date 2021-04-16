2ac939518ac09fdc51a6ae51c852b0631117531a
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-16 13:39:38 -0600

    SERVICIOS 3

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/RestEngine.kt
@@ -0,0 +1,23 @@
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RestEngine {
+    companion object{
+        fun buildService(): Retrofit{
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://192.168.1.74:8080/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit
+        }
+    }
+}
