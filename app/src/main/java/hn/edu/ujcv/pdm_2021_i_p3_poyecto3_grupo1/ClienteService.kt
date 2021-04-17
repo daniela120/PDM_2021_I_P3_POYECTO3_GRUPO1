86168345c7335d9af8109122f2c57d680046199f
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-04-17 05:21:33 -0600

    cliente services

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/ClienteService.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/ClienteService.kt
@@ -7,15 +7,15 @@ import retrofit2.http.*
 
 interface ClienteService {
     @GET("clientes")
-    fun listPersons(): Call<List<ClienteDataCollectionItem>>
+    fun listCliente(): Call<List<ClienteDataCollectionItem>>
     @GET("clientes/id/{id}")
-    fun getPersonById(@Path("id") id: Long): Call<ClienteDataCollectionItem>
+    fun getClienteById(@Path("id") id: Long): Call<ClienteDataCollectionItem>
     @Headers("Content-Type: application/json")
     @POST("clientes/addCliente")
-    fun addPerson(@Body personData: ClienteDataCollectionItem): Call<ClienteDataCollectionItem>
+    fun addCliente(@Body personData: ClienteDataCollectionItem): Call<ClienteDataCollectionItem>
     @Headers("Content-Type: application/json")
     @PUT("clientes")
-    fun updatePerson(@Body personData: ClienteDataCollectionItem): Call<ClienteDataCollectionItem>
+    fun updateCliente(@Body personData: ClienteDataCollectionItem): Call<ClienteDataCollectionItem>
     @DELETE("clientes/delete/{id}")
-    fun deletePerson(@Path("id") id: Long): Call<ResponseBody>
+    fun deleteCliente(@Path("id") id: Long): Call<ResponseBody>
 }
