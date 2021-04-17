59b7991de12150050c7d08cdf8ef41586fddb5a2
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-16 22:52:34 -0600

    SERVICIOS 4

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Empleado.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Empleado.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ClienteDataCollectionItem
+
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.EmpleadoDataCollectionItem
 import kotlinx.android.synthetic.main.activity_empleado.*
 import kotlinx.android.synthetic.main.activity_productos.*
 import retrofit2.Call
@@ -16,10 +18,64 @@ class Empleado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empleado)
-        val botonGetId = findViewById<Button>(R.id.btn_BuscarEmpleado)
-        botonGetId.setOnClickListener {v -> callServiceGetPerson()}
+        val botonguardar = findViewById<Button>(R.id.btn_BuscarEmpleado)
+        botonguardar.setOnClickListener {v -> callServicePostPerson()}
+
+
 
     }
+
+
+    private fun callServicePostPerson() {
+        val fecha = "2021-04-10"
+        val personInfo = EmpleadoDataCollectionItem(id = null,
+            nombrecompleto = txt_NombreEmpleado.text.toString(),
+            telefono = txt_TelefonoEmpleado.text.toString(),
+            correo = txt_CorreoCliente.text.toString(),
+            direccion = "EL SAUCE",
+            dni = "1234",
+            rtn = "10332"
+
+
+        )
+
+        addPerson(personInfo) {
+            if (it?.id != null) {
+                Toast.makeText(this@Empleado,"OK"+it?.id,Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this@Empleado,"Error",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    fun addPerson(personData: EmpleadoDataCollectionItem, onResult: (EmpleadoDataCollectionItem?) -> Unit){
+        val retrofit = RestEngine.buildService().create(PersonService::class.java)
+        var result: Call<EmpleadoDataCollectionItem> = retrofit.addPerson(personData)
+
+        result.enqueue(object : Callback<EmpleadoDataCollectionItem> {
+            override fun onFailure(call: Call<EmpleadoDataCollectionItem>, t: Throwable) {
+                onResult(null)
+            }
+
+            override fun onResponse(call: Call<EmpleadoDataCollectionItem>,
+                                    response: Response<EmpleadoDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val addedPerson = response.body()!!
+                    onResult(addedPerson)
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@Empleado,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@Empleado,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        )
+    }
+
 
     /*private  fun guardar() {
 
@@ -51,22 +107,6 @@ class Empleado : AppCompatActivity() {
         startActivity(intent)
     }*/
 
-    private fun callServiceGetPerson() {
-        val personService:PersonService = RestEngine.buildService().create(PersonService::class.java)
-        var result: Call<ClienteDataCollectionItem> = personService.getPersonById(1)
 
-        result.enqueue(object : Callback<ClienteDataCollectionItem> {
-            override fun onFailure(call: Call<ClienteDataCollectionItem>, t: Throwable) {
-                Toast.makeText(this@Empleado,"Error",Toast.LENGTH_LONG).show()
-            }
-
-            override fun onResponse(
-                call: Call<ClienteDataCollectionItem>,
-                response: Response<ClienteDataCollectionItem>
-            ) {
-                Toast.makeText(this@Empleado,"OK"+response.body()!!.nombrecompleto,Toast.LENGTH_LONG).show()
-            }
-        })
-    }
 
 }
