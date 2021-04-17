34f2fbe12cfa9de4be52620119fa3cb62b95def8
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-17 05:16:21 -0600

    COMPRAS

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Compras.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Compras.kt
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.getbase.floatingactionbutton.FloatingActionButton
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ClienteDataCollectionItem
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ComprasDataCollectionItem
 import kotlinx.android.synthetic.main.activity_cliente.*
 
 import kotlinx.android.synthetic.main.activity_compras.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Compras : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,23 +25,59 @@ class Compras : AppCompatActivity() {
 
     }
 
-    /*private  fun guardar() {
 
-        if (txt_IdCompra.text.toString().isEmpty()) {
-            Toast.makeText(this, "Ingrese ID de Compra", Toast.LENGTH_SHORT).show()
-        }else {
-            if (txt_TipoCompra.text.toString().isEmpty()) {
-                Toast.makeText(this, "Ingrese un tipo de compra", Toast.LENGTH_SHORT).show()
+
+    private fun callServicePostCompra() {
+
+        val compraInfo = ComprasDataCollectionItem(
+                cai = txt_CaiCompra.text.toString(),
+                proveedores = txt_TelefonoCliente.text.toString().toLong(),
+                numerotarjeta = txt_dniCliente.text.toString().toLong(),
+                formapago = txt_dniCliente.text.toString(),
+                fechaentrega = txt_CorreoCliente.text.toString(),
+                fechacompra = txt_DireccionCliente.text.toString(),
+                insumos = txt_ins
+        )
+
+        addPerson(personInfo) {
+            if (it?.id != null) {
+                Toast.makeText(this@MainActivity,"OK"+it?.id,Toast.LENGTH_LONG).show()
             } else {
-                if (txt_CantidadCompra.text.toString().isEmpty()) {
-                    Toast.makeText(this, "Ingrese una cantidad", Toast.LENGTH_SHORT).show()
-                } else{
-                    Toast.makeText(this, "Realizada con exito!", Toast.LENGTH_SHORT).show()
-
-                }
+                Toast.makeText(this@MainActivity,"Error",Toast.LENGTH_LONG).show()
             }
         }
-    }*/
+    }
+
+
+    fun addPerson(clienteData: ClienteDataCollectionItem, onResult: (ClienteDataCollectionItem?) -> Unit){
+        val retrofit = RestEngine.buildService().create(Cliente::class.java)
+        var result: Call<PersonDataCollectionItem> = retrofit.addPerson(personData)
+
+        result.enqueue(object : Callback<PersonDataCollectionItem> {
+            override fun onFailure(call: Call<PersonDataCollectionItem>, t: Throwable) {
+                onResult(null)
+            }
+
+            override fun onResponse(call: Call<PersonDataCollectionItem>,
+                                    response: Response<PersonDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val addedPerson = response.body()!!
+                    onResult(addedPerson)
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@MainActivity,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@MainActivity,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        )
+    }
+
+
+
 
     private fun Regresar() {
         val intent = Intent(this, Menu::class.java)
