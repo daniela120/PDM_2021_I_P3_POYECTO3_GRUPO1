2f3db7d988cdf2e210c66b6c5cff49ba8bcb66a9
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-16 23:29:23 -0600

    botones regresar y mostrar

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarProductos.kt
@@ -0,0 +1,16 @@
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_mostrar_productos.*
+
+class MostrarProductos:  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_productos)
+
+
+    }
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarTipoPago.kt
@@ -0,0 +1,17 @@
+
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_mostrar_tipo_pago.*
+
+class MostrarTipoPago:  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_tipo_pago)
+
+
+    }
+}
