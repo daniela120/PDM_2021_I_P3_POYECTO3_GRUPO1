e180ebd228f62866274f303b6d11cd44c966a163
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-18 11:52:59 -0600

    boton confirmar produccion

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarDetalleVenta.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarDetalleVenta.kt
@@ -3,28 +3,28 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.getbase.floatingactionbutton.FloatingActionButton
-import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.DepartamentoDataCollectionItem
+import kotlinx.android.synthetic.main.activity_departamento.*
+import kotlinx.android.synthetic.main.activity_mostrar_cliente.*
 import kotlinx.android.synthetic.main.activity_mostrar_detalle_ventas.*
-
+import kotlinx.android.synthetic.main.activity_mostrar_produccion.*
+import kotlinx.android.synthetic.main.activity_produccion.*
+import okhttp3.ResponseBody
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.VentaDetalleDataCollectionItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 class MostrarDetalleVenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_detalle_ventas)
-        btn_regresarFactura2.setOnClickListener { Regresar() }
 
+        val botonGetId = findViewById<Button>(R.id.btn_BuscarFactura)
 
-    }
-
-
-    private fun Regresar() {
-        val intent = Intent(this, DetalleVenta::class.java)
-        startActivity(intent)
     }
 
 }
