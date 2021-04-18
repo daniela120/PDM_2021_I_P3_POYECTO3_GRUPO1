80a67db19b89b96eaacf7519536eb2127fb5deb2
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-18 12:53:49 -0600

    Detalle Venta

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/DetalleVenta.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/DetalleVenta.kt
@@ -3,10 +3,18 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import com.getbase.floatingactionbutton.FloatingActionButton
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.VentasDataCollectionItem
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ProductoDataCollectionItem
 import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.VentaDetalleDataCollectionItem
 import kotlinx.android.synthetic.main.activity_detalle_ventas.*
+import kotlinx.android.synthetic.main.activity_empleado.*
+import kotlinx.android.synthetic.main.activity_produccion.*
 
 import retrofit2.Call
 import retrofit2.Callback
@@ -19,6 +27,11 @@ class DetalleVenta : AppCompatActivity() {
         btn_regresarFactura.setOnClickListener { Regresar() }
         findViewById<FloatingActionButton>(R.id.idFabListar_DetalleVenta).setOnClickListener {
             Mostrar()}
+        findViewById<FloatingActionButton>(R.id.idFabConfirmar_DetalleVenta).setOnClickListener {
+            callServicePostVentaDetalle() }
+        callServiceGetVenta()
+        callServiceGetProducto()
+
     }
 
 
@@ -32,4 +45,214 @@ class DetalleVenta : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun callServicePostVentaDetalle() {
+
+        val VentaDetalleInfo = VentaDetalleDataCollectionItem(id = null,
+                precio = txt_PrecioFactura1.text.toString().toLong(),
+                cantidad = txt_CantidadFactura1.text.toString().toLong(),
+                idventa = spinnerIdVenta1.selectedItemId.toString().toLong(),
+                idproducto = spinnerIdproductoFac1.selectedItemId.toString().toLong()
+
+        )
+
+        addVentaDetalle(VentaDetalleInfo) {
+            if (it?.id != null) {
+                Toast.makeText(this@DetalleVenta, "EMPLEADO AGREGADO", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this@DetalleVenta, "Error", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+    fun addVentaDetalle(VentaDetalleData: VentaDetalleDataCollectionItem, onResult: (VentaDetalleDataCollectionItem?) -> Unit){
+        val retrofit = RestEngine.buildService().create(DetalleVentaService::class.java)
+        var result: Call<VentaDetalleDataCollectionItem> = retrofit.addVentaDetalle(VentaDetalleData)
+
+        result.enqueue(object : Callback<VentaDetalleDataCollectionItem> {
+            override fun onFailure(call: Call<VentaDetalleDataCollectionItem>, t: Throwable) {
+                onResult(null)
+            }
+
+            override fun onResponse(call: Call<VentaDetalleDataCollectionItem>,
+                                    response: Response<VentaDetalleDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val addedVentaDetalle = response.body()!!
+                    onResult(addedVentaDetalle)
+                } else if (response.code() == 401) {
+                    Toast.makeText(this@DetalleVenta, "Sesion expirada", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this@DetalleVenta, "Fallo al traer el item", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        )
+    }
+
+    /*SPINNER VENTA*/
+    private fun callServiceGetVenta() {
+        var lista: java.util.HashSet<String> = hashSetOf()
+
+        val tipoService: VentasService= RestEngine.buildService().create(VentasService::class.java)
+        var result: Call<List<VentasDataCollectionItem>> = tipoService.listVentas()
+
+        result.enqueue(object : Callback<List<VentasDataCollectionItem>> {
+            override fun onFailure(call: Call<List<VentasDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@DetalleVenta, "Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                    call: Call<List<VentasDataCollectionItem>>,
+                    response: Response<List<VentasDataCollectionItem>>
+            ) {
+                try {
+                    for (i in response.body()!!) {
+                        lista.add(i.id.toString())
+                    }
+
+                    iniciar1(lista)
+
+                } catch (e: Exception) {
+                    println("No hay datos de la venta")
+
+                }
+
+            }
+        })
+
+    }
+
+    fun iniciar1(a: java.util.HashSet<String>){
+        val spinnerventa = findViewById<Spinner>(R.id.spinnerIdVenta1)
+        var valor:String
+        var A: java.util.ArrayList<String> = java.util.ArrayList()
+        for(i in a){
+            val data = i.toString().split("|")
+            valor=data[0].toString()
+            A.add(valor)
+
+        }
+
+        val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,A)
+
+        spinnerventa.adapter =adaptador
+        spinnerventa.onItemSelectedListener = object:
+                AdapterView.OnItemSelectedListener { override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+                callServiceGetVentaById(A[position].toString().toLong())
+
+
+            }
+        }
+
+    }
+
+    private fun callServiceGetVentaById(a:Long) {
+        var idven=""
+        val pagoservice: VentasService = RestEngine.buildService().create(VentasService::class.java)
+        var result: Call<VentasDataCollectionItem> = pagoservice.getVentasById(a)
+
+        result.enqueue(object : Callback<VentasDataCollectionItem> {
+            override fun onFailure(call: Call<VentasDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@DetalleVenta,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                    call: Call<VentasDataCollectionItem>,
+                    response: Response<VentasDataCollectionItem>
+            ) {
+                idven = response.body()!!.id.toString()
+                txv_SeleccionVentaId1.text = idven
+
+            }
+        })
+
+
+    }/*SPINER PRODUCTO*/
+
+    private fun callServiceGetProducto() {
+        var lista: java.util.HashSet<String> = hashSetOf()
+
+        val tipoService: ProductoService = RestEngine.buildService().create(ProductoService::class.java)
+        var result: Call<List<ProductoDataCollectionItem>> = tipoService.listProductos()
+
+        result.enqueue(object : Callback<List<ProductoDataCollectionItem>> {
+            override fun onFailure(call: Call<List<ProductoDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@DetalleVenta, "Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                    call: Call<List<ProductoDataCollectionItem>>,
+                    response: Response<List<ProductoDataCollectionItem>>
+            ) {
+                try {
+                    for (i in response.body()!!) {
+                        lista.add(i.id.toString())
+                    }
+
+                    iniciar2(lista)
+
+                } catch (e: Exception) {
+                    println("No hay datos del producto")
+
+                }
+
+            }
+        })
+
+    }
+
+    fun iniciar2(a: java.util.HashSet<String>){
+        val spinner_Producto = findViewById<Spinner>(R.id.spinnerIdproductoFac1)
+        var valor:String
+        var A: java.util.ArrayList<String> = java.util.ArrayList()
+        for(i in a){
+            val data = i.toString().split("|")
+            valor=data[0].toString()
+            A.add(valor)
+
+        }
+
+        val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,A)
+
+        spinner_Producto.adapter =adaptador
+        spinner_Producto.onItemSelectedListener = object:
+                AdapterView.OnItemSelectedListener { override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+                callServiceGetProductoById(A[position].toString().toLong())
+
+
+            }
+        }
+
+    }
+
+    private fun callServiceGetProductoById(a:Long) {
+        var idpro=""
+        val productoservice: ProductoService = RestEngine.buildService().create(ProductoService::class.java)
+        var result: Call<ProductoDataCollectionItem> = productoservice.getProductoById(a)
+
+        result.enqueue(object : Callback<ProductoDataCollectionItem> {
+            override fun onFailure(call: Call<ProductoDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@DetalleVenta,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                    call: Call<ProductoDataCollectionItem>,
+                    response: Response<ProductoDataCollectionItem>
+            ) {
+                idpro = response.body()!!.id.toString()
+                txv_select3.text = idpro
+
+            }
+        })
+
+
+    }
 }
