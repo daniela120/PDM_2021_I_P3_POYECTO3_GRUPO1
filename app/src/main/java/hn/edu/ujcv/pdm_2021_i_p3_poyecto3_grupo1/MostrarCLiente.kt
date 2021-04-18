31727891b5184c9ffed23c8d9c48ce7fa80eb955
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-17 22:44:34 -0600

    POST AND DELETE

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarCLiente.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarCLiente.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.Toast
 import com.getbase.floatingactionbutton.FloatingActionButton
 import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ClienteDataCollectionItem
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.EmpleadoDataCollectionItem
 import kotlinx.android.synthetic.main.activity_cliente.*
 import kotlinx.android.synthetic.main.activity_mostrar_cliente.*
 import okhttp3.ResponseBody
@@ -26,6 +27,9 @@ class MostrarCLiente:  AppCompatActivity() {
         btn_regresarCliente2.setOnClickListener { Regresar() }
         findViewById<FloatingActionButton>(R.id.idFabEliminar_Cli).setOnClickListener{
             callServiceDeletePerson()
+        }
+        findViewById<FloatingActionButton>(R.id.idFabActualizar_Cli).setOnClickListener{
+            callServicePutCliente()
         }
 
 
@@ -134,6 +138,44 @@ class MostrarCLiente:  AppCompatActivity() {
             Toast.makeText(this@MostrarCLiente,"NO SE PUEDO ELIMINAR EL CLIENTE CON EL ID: "+ txt_IdCliente2.text.toString(),Toast.LENGTH_LONG).show()
 
         }    }
+
+
+    private fun callServicePutCliente() {
+
+        val clienteInfo = ClienteDataCollectionItem(  id = txt_IdCliente2.text.toString().toLong(),
+                nombrecompleto = txt_NombreCliente2.text.toString(),
+                telefono = txt_TelefonoCliente2.text.toString().toLong(),
+                dni = txt_dniCliente2.text.toString().toLong(),
+                rtn = txt_rtnCliente2.text.toString(),
+                correo = txt_CorreoCliente2.text.toString(),
+                direccion = txt_DireccionCliente2.text.toString()
+
+        )
+
+        val retrofit = RestEngine.buildService().create(ClienteService::class.java)
+        var result: Call<ClienteDataCollectionItem> = retrofit.updateCliente(clienteInfo)
+
+        result.enqueue(object : Callback<ClienteDataCollectionItem> {
+            override fun onFailure(call: Call<ClienteDataCollectionItem>, t: Throwable) {
+                Toast.makeText(this@MostrarCLiente,"Error",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<ClienteDataCollectionItem>,
+                                    response: Response<ClienteDataCollectionItem>) {
+                if (response.isSuccessful) {
+                    val updatedPerson = response.body()!!
+                    Toast.makeText(this@MostrarCLiente,"OK"+response.body()!!.nombrecompleto,Toast.LENGTH_LONG).show()
+                }
+                else if (response.code() == 401){
+                    Toast.makeText(this@MostrarCLiente,"Sesion expirada",Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this@MostrarCLiente,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
+    }
 
 
 
