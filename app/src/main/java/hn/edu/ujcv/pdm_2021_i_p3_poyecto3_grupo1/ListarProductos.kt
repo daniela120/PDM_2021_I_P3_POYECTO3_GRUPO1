d174b5d5bda4c2c6b9c6ea325970d6d3ebaac75c
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-04-19 14:09:17 -0600

    LISVIEW

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/ListarProductos.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/ListarProductos.kt
@@ -3,17 +3,98 @@ package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.InsumosDataCollectionItem
+import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.ProductoDataCollectionItem
 import kotlinx.android.synthetic.main.activity_listar_productos.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
 class ListarProductos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listar_productos)
         btn_regresarListarProducto.setOnClickListener { Regresar() }
+        callServiceGetListaPROD()
     }
 
     private fun Regresar() {
         val intent = Intent(this, Productos::class.java)
         startActivity(intent)
+    }
+    private fun callServiceGetListaPROD() {
+        var lista: java.util.HashSet<String> = hashSetOf()
+
+        val tipoService:ProductoService= RestEngine.buildService().create(ProductoService ::class.java)
+        var result: Call<List<ProductoDataCollectionItem>> = tipoService.listProductos()
+
+        result.enqueue(object : Callback<List<ProductoDataCollectionItem>> {
+            override fun onFailure(call: Call<List<ProductoDataCollectionItem>>, t: Throwable) {
+                Toast.makeText(this@ListarProductos ,"Error", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(
+                    call: Call<List<ProductoDataCollectionItem>>,
+                    response: Response<List<ProductoDataCollectionItem>>
+            ) {
+                try {
+                    for (i in response.body()!!) {
+                        lista.add("  "+i.id.toString()+"                    "+i.nombre+"                            " +i.descripcion)
+                    }
+
+
+                    iniciar2(lista)
+
+                } catch (e: Exception) {
+                    println("No hay datos del producto")
+
+                }
+
+            }
+        })
+
+    }
+
+    fun iniciar2(a: java.util.HashSet<String>){
+        val list = findViewById<ListView>(R.id.list_productos)
+
+        var valor:String
+
+        var A: java.util.ArrayList<String> = java.util.ArrayList()
+
+        for(i in a){
+            val data = i.toString().split(" | ")
+            valor=data[0].toString()
+            A.add(valor)
+
+        }
+
+
+
+
+        val adaptador = ArrayAdapter(this,android.R.layout.simple_list_item_1,A)
+
+        list.adapter =adaptador
+        list.onItemSelectedListener = object:
+                AdapterView.OnItemSelectedListener { override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+            {
+
+
+
+            }
+        }
+
+
+
+
     }
 }
