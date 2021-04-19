4169f70bb0590377c9c23f7a7c8bcfea340adc7b
Edwin Rafael <edwin.espino@ujcv.edu>
2021-04-18 18:25:44 -0600

    VENTAS CRUD

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarVentas.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/MostrarVentas.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.getbase.floatingactionbutton.FloatingActionButton
 import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.CLIENTES.ClienteService
 import hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1.entities.*
 import kotlinx.android.synthetic.main.activity_cliente.*
+import kotlinx.android.synthetic.main.activity_mostrar_empleado.*
+import kotlinx.android.synthetic.main.activity_mostrar_insumos.*
 import kotlinx.android.synthetic.main.activity_mostrar_ventas.*
 import kotlinx.android.synthetic.main.activity_ventas.*
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,6 +27,14 @@ class MostrarVentas : AppCompatActivity() {
         btn_regresarVentas2.setOnClickListener { Regresar()}
         val botonGetId = findViewById<Button>(R.id.btn_BuscarVenta)
         botonGetId.setOnClickListener {v -> callServiceGetPerson()}
+        findViewById<FloatingActionButton>(R.id.idFabActualizar_Ventas).setOnClickListener {
+            callServicePutVentas()
+        }
+
+        findViewById<FloatingActionButton>(R.id.idFabEliminar_Ventas).setOnClickListener {
+            callServiceDeleteVenta()
+        }
+
 
         callServiceGetTipo()
         callServiceGetEmpleados()
@@ -37,18 +49,50 @@ class MostrarVentas : AppCompatActivity() {
 
     private fun callServiceGetPerson() {
         val ventasService:VentasService = RestEngine.buildService().create(VentasService::class.java)
-        var result: Call<VentasDataCollectionItem> = ventasService.getVentasById(1)
+        var result: Call<VentasDataCollectionItem> = ventasService.getVentasById(txt_VentaId2.text.toString().toLong())
 
         result.enqueue(object : Callback<VentasDataCollectionItem> {
             override fun onFailure(call: Call<VentasDataCollectionItem>, t: Throwable) {
-                Toast.makeText(this@MostrarVentas,"Error", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MostrarVentas, "Error", Toast.LENGTH_LONG).show()
             }
 
             override fun onResponse(
                     call: Call<VentasDataCollectionItem>,
                     response: Response<VentasDataCollectionItem>
             ) {
-                Toast.makeText(this@MostrarVentas,"OK"+response.body()!!.cai, Toast.LENGTH_LONG).show()
+                try {
+                    var a = response.body()!!.descripcion
+                    var b = response.body()!!.idempleado
+                    var c = response.body()!!.cai
+                    var d = response.body()!!.idcliente
+                    var e = response.body()!!.numerotarjeta
+                    var f = response.body()!!.formadepago
+                    var g = response.body()!!.fechaventa
+                    var h = response.body()!!.fechaentrega
+
+                    txt_DescripcionVenta2.setText(a)
+                    txv_SeleccionEmp.setText(b.toString())
+                    txt_CaiVenta2.setText(c.toString())
+                    txv_SeleccionCliente.setText(d.toString())
+                    txt_NoTarjetaVentaM.setText(e.toString())
+                    txv_SeleccionTipo.setText(f.toString())
+                    txt_FechaVentaM.setText(g)
+                    txt_FechaEntre.setText(h)
+
+                    txt_DescripcionVenta2.isEnabled = true
+                    txv_SeleccionEmp.isEnabled = true
+                    txt_CaiVenta2.isEnabled = true
+                    txv_SeleccionCliente.isEnabled = true
+                    txt_NoTarjetaVentaM.isEnabled = true
+                    txv_SeleccionTipo.isEnabled = true
+                    txt_FechaVentaM.isEnabled = true
+                    txt_FechaEntre.isEnabled = true
+
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@MostrarVentas, "ERROR VERIFIQUE LOS DATOS", Toast.LENGTH_SHORT).show()
+                    reset()
+                }
             }
         })
     }
@@ -399,6 +443,108 @@ class MostrarVentas : AppCompatActivity() {
 
 
     }
+
+    fun reset(){
+        txt_VentaId2.setText("")
+        txt_DescripcionVenta2.setText("")
+        txv_SeleccionEmp.setText("")
+        txt_CaiVenta2.setText("")
+        txv_SeleccionCliente.setText("")
+        txt_NoTarjetaVentaM.setText("")
+        txv_SeleccionTipo.setText("")
+        txt_FechaVentaM.setText("")
+        txt_FechaEntre.setText("")
+
+        txt_DescripcionVenta2.isEnabled = false
+        txv_SeleccionEmp.isEnabled = false
+        txt_CaiVenta2.isEnabled = false
+        txv_SeleccionCliente.isEnabled = false
+        txt_NoTarjetaVentaM.isEnabled = false
+        txv_SeleccionTipo.isEnabled = false
+        txt_FechaVentaM.isEnabled = false
+        txt_FechaEntre.isEnabled = false
+
+    }
+
+
+
+    //Actualizar
+    private fun callServicePutVentas() {
+        try {
+
+            val ventainfo = VentasDataCollectionItem(id = txt_VentaId2.text.toString().toLong(),
+                    descripcion = txt_DescripcionVenta2.text.toString(),
+                    idempleado = spinnerIdEmpleadoM.selectedItem.toString().toLong(),
+                    cai = txt_CaiVenta2.text.toString().toLong(),
+                    idcliente = spinnercCLM.selectedItem.toString().toLong(),
+                    numerotarjeta = txt_NoTarjetaVentaM.text.toString().toLong(),
+                    formadepago = spinnerFormaPagoM.selectedItem.toString().toLong(),
+                    fechaventa = txt_FechaVentaM.text.toString(),
+                    fechaentrega = txt_FechaEntre.text.toString()
+            )
+
+
+            val retrofit = RestEngine.buildService().create(VentasService::class.java)
+            var result: Call<VentasDataCollectionItem> = retrofit.updateVentas(ventainfo)
+
+
+            result.enqueue(object : Callback<VentasDataCollectionItem> {
+                override fun onFailure(call: Call<VentasDataCollectionItem>, t: Throwable) {
+                    Toast.makeText(this@MostrarVentas, "Error", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(call: Call<VentasDataCollectionItem>,
+                                        response: Response<VentasDataCollectionItem>) {
+                    if (response.isSuccessful) {
+                        val updatedPerson = response.body()!!
+                        Toast.makeText(this@MostrarVentas, " VENTA ACTUALIZADA", Toast.LENGTH_LONG).show()
+                    } else if (response.code() == 401) {
+                        Toast.makeText(this@MostrarVentas, "Sesion expirada", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this@MostrarVentas, "Fallo al traer el item", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            })
+        }catch (e:Exception){
+            Toast.makeText(this@MostrarVentas, "ERROR, POR FAVOR VERIFIQUE LOS DATOS", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    //Metodo Delete
+    private fun callServiceDeleteVenta() {
+        try {
+
+
+            val ventaService: VentasService = RestEngine.buildService().create(VentasService::class.java)
+            var result: Call<ResponseBody> = ventaService.deleteVentas(txt_VentaId2.text.toString().toLong())
+
+            result.enqueue(object :  Callback<ResponseBody> {
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Toast.makeText(this@MostrarVentas,"Error",Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        reset()
+                        Toast.makeText(this@MostrarVentas,"VENTA ELIMINADA",Toast.LENGTH_LONG).show()
+                    }
+                    else if (response.code() == 401){
+                        Toast.makeText(this@MostrarVentas,"Sesion expirada",Toast.LENGTH_LONG).show()
+                    }
+                    else{
+                        Toast.makeText(this@MostrarVentas,"Fallo al traer el item",Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+        }catch (e: java.lang.Exception){
+            Toast.makeText(this@MostrarVentas,"NO SE PUEDO ELIMINAR LA VENTA CON EL ID: "+ txt_IdInsumo2.text.toString(),Toast.LENGTH_LONG).show()
+            reset()
+        }    }
+
 
 
 
