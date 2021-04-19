a63954bdf584a837d839ffd341413d5a129208c6
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-04-18 22:33:32 -0600

    Activitys Listar

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/ListarInsumos.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_p3_poyecto3_grupo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListarInsumos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_insumos)
+    }
+}
