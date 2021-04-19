5313a6685d358269b9fb445fa8333a13dd38e733
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-04-19 17:24:15 -0600

    LISVIEW

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Produccion.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p3_poyecto3_grupo1/Produccion.kt
@@ -115,7 +115,7 @@ class Produccion : AppCompatActivity() {
             ) {
                 try {
                     for (i in response.body()!!) {
-                        lista.add(i.id.toString() + " " + i.descripcion)
+                        lista.add(i.id.toString())
                     }
 
                     iniciar2(lista)
